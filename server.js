d9c75fa33caf9ee42b8a77fd5478b7ac93e84438
EnriqueDarioGonzalez <enrii.xd6@gmail.com>
2024-01-11 18:42:58 -0300

    update11

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ const productsRouter = require('./src/routes/products.router.js')
 const cartRouter = require('./src/routes/cart.router.js');
 const handlebars = require('express-handlebars')
 const viewsRouter = require('./src/routes/views.router.js')
-const userRouter = require('./src/routes/users.routers.js')
+const userRouter = require('./src/routes/users.router.js')
 const { Server } = require('socket.io');
 const { connectDb } = require('./src/config/index.js');
 const bodyParser = require('body-parser');
+const session = require('express-session')
+const sessionsRouter = require('./src/routes/sessions.router.js')
+const MongoStore = require('connect-mongo')
 
 const app = express();
 const puerto = 8080;
@@ -32,10 +35,42 @@ app.use('/api/views', viewsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
+// app.use('/api/orders', ordersRouter)
+app.use('/api/sessions', sessionsRouter)
   app.use(( err, req, res, next)=>{
     console.error(err.stack)
     res.status(500).send('error de server')
 })
+
+
+// const fileStore = new FileStore(session)
+// app.use(session({
+//     store: new fileStore({
+//         path: './sessions',
+//         ttl: 100,
+//         retire: 0
+//     }),
+//     secret: 'secretCoder',
+//     resave: true,
+//     saveUninitialized: true
+// }))
+
+
+
+// estrategia de session con mongo
+app.use(session({
+  store: MongoStore.create({
+      mongoUrl: 'mongodb://127.0.0.1:27017/miPrimeraBase',
+      mongoOptions: {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+      },
+      ttl: 15000000000,
+  }),
+  secret: 'secretCoder',
+  resave: true,
+  saveUninitialized: true
+}))
 
 const serverHttp = app.listen(puerto, () => console.log(`Servidor escuchando en el ${puerto}`));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,14 @@ const app = require('./server');
 const productsRouter = require('./src/routes/products.router.js');
 const cartRouter = require('./src/routes/cart.router.js');
 const viewsRouter = require('./src/routes/views.router.js');
-const userRouter = require('./src/routes/users.router.js');
+const usersRouter = require('./users.router.js');
 
 // Rutas API
 app.use('/api/views', viewsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/products', productsRouter);
 app.use('/api/cart', cartRouter);
+app.use('/api/users', usersRouter);
 
 // Manejador de errores
 app.use((err, req, res, next) => {
